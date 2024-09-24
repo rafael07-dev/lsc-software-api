e12acd499aa92e8de34e40cb428b1b7d42475b1c
rafael07-dev <deralato817@gmail.com>
2024-09-24 10:50:38 -0500

    add method delete words by id

--- src/main/java/com/lsc/software/api/controller/WordController.java
+++ src/main/java/com/lsc/software/api/controller/WordController.java
@@ -1,11 +1,14 @@
 package com.lsc.software.api.controller;
 
 import com.lsc.software.api.model.Word;
+import com.lsc.software.api.response.ResponseApi;
 import com.lsc.software.api.service.WordService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/words")
@@ -22,8 +25,19 @@ public class WordController {
         return ResponseEntity.ok().body(wordService.getAllWords(letter));
     }
 
-    /*@GetMapping("/{letter}")
-    public ResponseEntity<List<Word>> getWordsByLetter(@RequestParam Long letter) {
-        return ResponseEntity.ok().body(wordService.findWordById(idLetter));
-    }*/
+    @GetMapping("/{idWord}")
+    public ResponseEntity<Optional<Word>> findById(@PathVariable Long idWord) {
+        return ResponseEntity.ok().body(wordService.findById(idWord));
+    }
+
+    @PostMapping(value = "/create", consumes = "application/json")
+    public ResponseEntity<Word> addWord(@RequestBody @Valid Word word) {
+        return ResponseEntity.created(URI.create("/api/words/" + word.getId()))
+                .body(wordService.saveWord(word));
+    }
+
+    @DeleteMapping("/delete/{idWord}")
+    public ResponseEntity<ResponseApi> deleteWord(@PathVariable Long idWord) {
+        return ResponseEntity.ok().body(wordService.deleteWord(idWord));
+    }
 }
