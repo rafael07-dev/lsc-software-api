e50b826a3c257bf439f14428ceb6ce660abddcda
rafael07-dev <deralato817@gmail.com>
2024-10-07 10:29:26 -0500

    add jwt service, and filter

--- src/main/java/com/lsc/software/api/security/JwtTokenFilter.java
+++ src/main/java/com/lsc/software/api/security/JwtTokenFilter.java
@@ -1,2 +1,53 @@
-package com.lsc.software.api.security;public class JwtTokenFilter {
+package com.lsc.software.api.security;
+
+import com.lsc.software.api.service.AuthService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private final AuthService authService;
+
+    public JwtTokenFilter(JwtTokenProvider jwtTokenProvider, AuthService authService) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authService = authService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwtToken = null;
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            jwtToken = authHeader.substring(7);
+
+            if (jwtTokenProvider.validateToken(jwtToken)) {
+                username = jwtTokenProvider.getUsernameFromToken(jwtToken);
+
+                UserDetails userDetails = authService.loadUser(username);
+
+                if (userDetails != null){
+                    var authentication = jwtTokenProvider.getAuthentication(jwtToken, userDetails);
+
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
--- src/main/java/com/lsc/software/api/security/JwtTokenProvider.java
+++ src/main/java/com/lsc/software/api/security/JwtTokenProvider.java
@@ -1,2 +1,61 @@
-package com.lsc.software.api.security;public class JwtTokenProvider {
+package com.lsc.software.api.security;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import java.util.*;
+
+@Component
+public class JwtTokenProvider {
+
+    private final SecretKey secretKey = Jwts.SIG.HS256.key().build();
+
+    public String createToken(String subject, String roles) {
+        Map<String, Object> claimsMap = new HashMap<>();
+        claimsMap.put("sub", subject);
+        claimsMap.put("roles", roles);
+
+        Date now = new Date();
+        long validityInSeconds = 3600000L;
+
+        Date expiration = new Date(now.getTime() + validityInSeconds);
+
+        return Jwts.builder()
+                .claims(claimsMap)
+                .expiration(expiration)
+                .signWith(secretKey)
+                .issuedAt(now)
+                .id(UUID.randomUUID().toString())
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token, UserDetails userDetails) {
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
 }
