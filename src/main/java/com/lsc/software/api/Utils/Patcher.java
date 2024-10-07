8d74322c78e99859c8b9528f458c445e6d241a6b
rafael07-dev <deralato817@gmail.com>
2024-10-07 10:36:50 -0500

    add patcher to compare fields of new objects and olds

--- src/main/java/com/lsc/software/api/Utils/Patcher.java
+++ src/main/java/com/lsc/software/api/Utils/Patcher.java
@@ -1,2 +1,27 @@
-package com.lsc.software.api.Utils;public class Patcher {
+package com.lsc.software.api.Utils;
+
+import com.lsc.software.api.model.UserEntity;
+import org.springframework.stereotype.Component;
+import java.lang.reflect.Field;
+
+@Component
+public class Patcher {
+
+    public void userPatcher(UserEntity existingUser, UserEntity newUser) throws IllegalAccessException {
+        Class<?> userClass = UserEntity.class;
+
+        Field[] userFields = userClass.getDeclaredFields();
+
+        for (Field userField : userFields) {
+            userField.setAccessible(true);
+
+            Object value = userField.get(newUser);
+
+            if (value != null) {
+                userField.set(existingUser, value);
+            }
+
+            userField.setAccessible(false);
+        }
+    }
 }
