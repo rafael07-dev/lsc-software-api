9f2b6a13156c65f511b840ff5cd0a88f736e818d
rafael07-dev <deralato817@gmail.com>
2024-10-07 10:34:11 -0500

    add objet to register

--- src/main/java/com/lsc/software/api/Dto/UserSingUp.java
+++ src/main/java/com/lsc/software/api/Dto/UserSingUp.java
@@ -1,2 +1,109 @@
-package com.lsc.software.api.Dto;public class UserSingUp {
+package com.lsc.software.api.Dto;
+
+import jakarta.validation.constraints.*;
+
+public class UserSingUp {
+
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Size(min = 2, max = 20)
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Size(min = 2, max = 20)
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Size(min = 2, max = 20)
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Size(min = 2, max = 20)
+    private String email;
+
+    @Email
+    @NotNull
+    @NotBlank
+    @NotEmpty
+    @Size(min = 2, max = 20)
+    private String password;
+
+    private String roles;
+
+    public UserSingUp() {
+
+    }
+
+    public UserSingUp(Long id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public @NotNull @NotBlank @NotEmpty @Size(min = 2, max = 20) String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(@NotNull @NotBlank @NotEmpty @Size(min = 2, max = 20) String firstName) {
+        this.firstName = firstName;
+    }
+
+    public @NotNull @NotBlank @NotEmpty @Size(min = 2, max = 20) String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(@NotNull @NotBlank @NotEmpty @Size(min = 2, max = 20) String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public @NotNull @NotBlank @NotEmpty @Size(min = 2, max = 20) String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotNull @NotBlank @NotEmpty @Size(min = 2, max = 20) String email) {
+        this.email = email;
+    }
+
+    public @Email @NotNull @NotBlank @NotEmpty @Size(min = 2, max = 20) String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@Email @NotNull @NotBlank @NotEmpty @Size(min = 2, max = 20) String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
