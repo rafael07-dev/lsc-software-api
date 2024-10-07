2afbe9726220b2abab433e05c02c43a7794c59d9
rafael07-dev <deralato817@gmail.com>
2024-10-07 10:40:59 -0500

    find user by username

--- src/main/java/com/lsc/software/api/repository/UserRepository.java
+++ src/main/java/com/lsc/software/api/repository/UserRepository.java
@@ -1,2 +1,11 @@
-package com.lsc.software.api.repository;public interface UserRepository {
+package com.lsc.software.api.repository;
+
+import com.lsc.software.api.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
 }
