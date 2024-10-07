5f84981efddec27b28b9d7e781f977d1753a75a1
rafael07-dev <deralato817@gmail.com>
2024-10-07 10:38:09 -0500

    add dto to login

--- src/main/java/com/lsc/software/api/model/UserAuth.java
+++ src/main/java/com/lsc/software/api/model/UserAuth.java
@@ -1,2 +1,31 @@
-package com.lsc.software.api.model;public class UserAuth {
+package com.lsc.software.api.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class UserAuth {
+
+    @NotBlank
+    @NotNull
+    private String username;
+
+    @NotBlank
+    @NotNull
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
