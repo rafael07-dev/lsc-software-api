2cf324e5347a1f5a430d9a1c22c93e5dd61ce100
rafael07-dev <deralato817@gmail.com>
2024-10-07 10:31:20 -0500

    configure spring security

--- src/main/java/com/lsc/software/api/config/SecurityConfig.java
+++ src/main/java/com/lsc/software/api/config/SecurityConfig.java
@@ -1,2 +1,55 @@
-package com.lsc.software.api.config;public class SecurityConfig {
+package com.lsc.software.api.config;
+
+import com.lsc.software.api.security.JwtTokenFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    public SecurityConfig(JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((auth) -> {
+                    auth.requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll();
+                    auth.requestMatchers(HttpMethod.POST, "/api/auth/logout").permitAll();
+                    auth.requestMatchers(HttpMethod.POST, "/api/auth/sing-up").permitAll();
+                    auth.requestMatchers("/api/users/*").hasAnyRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.PATCH, "/api/users/**").hasAnyRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.POST, "/api/admin/**").hasRole("ADMIN");
+                    auth.anyRequest().authenticated();
+                }).addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
