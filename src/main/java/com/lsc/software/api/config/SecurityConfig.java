cd17bfed37cfb691a7d7fff4f117423ad1e3da19
rafael07-dev <deralato817@gmail.com>
2025-05-15 09:04:40 -0500

    fix access to static files

--- src/main/java/com/lsc/software/api/config/SecurityConfig.java
+++ src/main/java/com/lsc/software/api/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.lsc.software.api.config;
 import com.lsc.software.api.security.JwtTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
@@ -16,7 +15,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.nio.file.Paths;
 
 @Configuration
@@ -26,7 +24,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     private final JwtTokenFilter jwtTokenFilter;
     private final AuthenticationProvider authenticationProvider;
 
-    @Lazy
+    //@Lazy
     public SecurityConfig(JwtTokenFilter jwtTokenFilter, AuthenticationProvider authenticationProvider) {
         this.jwtTokenFilter = jwtTokenFilter;
         this.authenticationProvider = authenticationProvider;
@@ -34,12 +32,16 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf(AbstractHttpConfigurer::disable)
+        return http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/auth/**").permitAll();
                     auth.requestMatchers("/api/login/**").permitAll();
+                    auth.requestMatchers("/api/logout/**").permitAll();
+                    auth.requestMatchers("/gifs/**").permitAll();
                     auth.requestMatchers("/api/users/**").hasRole("ADMIN");
                     auth.anyRequest().authenticated();
                 })
@@ -65,6 +67,6 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/gifs/**")
-                .addResourceLocations("file:" + Paths.get("uploads/gifs").toAbsolutePath().toUri());
+                .addResourceLocations("file:" + Paths.get("uploads/gifs").toAbsolutePath() + "/");
     }
 }
