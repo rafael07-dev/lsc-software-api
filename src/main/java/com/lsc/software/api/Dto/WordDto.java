77353df2e8f8d5a1b1fa507ab0955b6bca006089
rafael07-dev <deralato817@gmail.com>
2025-05-22 16:35:58 -0500

    add word dto

---
+++ src/main/java/com/lsc/software/api/Dto/WordDto.java
@@ -0,0 +1,17 @@
+package com.lsc.software.api.Dto;
+
+public class WordDto {
+
+    String word;
+    Long letter_id;
+
+
+    public String getWord() {
+        return word;
+    }
+
+    public Long getLetter_id() {
+        return letter_id;
+    }
+
+}
