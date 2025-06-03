544f887aa6b45741f25edeecbb40804ae8612c7e
rafael07-dev <deralato817@gmail.com>
2025-06-03 08:50:45 -0500

    add method to find words by letter

--- src/main/java/com/lsc/software/api/repository/WordRepository.java
+++ src/main/java/com/lsc/software/api/repository/WordRepository.java
@@ -1,6 +1,8 @@
 package com.lsc.software.api.repository;
 
 import com.lsc.software.api.model.Word;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface WordRepository extends JpaRepository<Word, Long> {
 
     Word findWordByWord(String word);
+    Page<Word> findByLetter_Letter(String letter, Pageable pageable);
 }
