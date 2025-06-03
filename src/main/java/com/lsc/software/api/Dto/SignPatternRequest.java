f7c3e9a45ac6c4463b0741cc7ae53800d0b82611
rafael07-dev <deralato817@gmail.com>
2025-06-03 08:49:54 -0500

    adding dtos sign pattern

--- src/main/java/com/lsc/software/api/Dto/SignPatternRequest.java
+++ src/main/java/com/lsc/software/api/Dto/SignPatternRequest.java
@@ -1,2 +1,17 @@
-package com.lsc.software.api.Dto;public class SignPatternRequest {
+package com.lsc.software.api.Dto;
+
+import java.util.List;
+
+public class SignPatternRequest {
+
+    private String word;
+
+    private List<List<List<Double>>> sequence;
+
+    // Getters y setters
+    public String getWord() { return word; }
+    public void setWord(String word) { this.word = word; }
+
+    public List<List<List<Double>>> getSequence() { return sequence; }
+    public void setSequence(List<List<List<Double>>> sequence) { this.sequence = sequence; }
 }
--- src/main/java/com/lsc/software/api/Dto/SignPatternResponse.java
+++ src/main/java/com/lsc/software/api/Dto/SignPatternResponse.java
@@ -1,2 +1,23 @@
-package com.lsc.software.api.Dto;public class SignPatternResponse {
+package com.lsc.software.api.Dto;
+
+import java.util.List;
+
+public class SignPatternResponse {
+
+    private final String word;
+    private final List<List<List<Double>>> pattern;
+
+    public SignPatternResponse(String word, List<List<List<Double>>> pattern) {
+        this.word = word;
+        this.pattern = pattern;
+    }
+
+
+    public String getWord() {
+        return word;
+    }
+
+    public List<List<List<Double>>> getPattern() {
+        return pattern;
+    }
 }
