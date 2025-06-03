e6a67795155871e513d1c6dd2a228ed6f9f4c583
rafael07-dev <deralato817@gmail.com>
2025-06-03 08:50:15 -0500

    add sign pattern repository

--- src/main/java/com/lsc/software/api/repository/SignPatternRepository.java
+++ src/main/java/com/lsc/software/api/repository/SignPatternRepository.java
@@ -1,2 +1,10 @@
-package com.lsc.software.api.repository;public interface SignPatternRepository {
+package com.lsc.software.api.repository;
+
+import com.lsc.software.api.model.SignPattern;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SignPatternRepository extends JpaRepository<SignPattern, Long> {
+    Optional<SignPattern> findByWord(String word);
 }
